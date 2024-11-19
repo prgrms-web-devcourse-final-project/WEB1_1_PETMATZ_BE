292d19db151e611bc42c5a16078e4671a483ecd1
최철진 <cil6879@gmail.com>
2024-11-19 11:12:01 +0900

    Feat : BaseEntity 생성

--- src/main/java/com/petmatz/domain/global/BaseEntity.java
+++ src/main/java/com/petmatz/domain/global/BaseEntity.java
@@ -1,34 +1,30 @@
 package com.petmatz.domain.global;//package spring.basic.domain.global;
-//
-//import jakarta.persistence.Column;
-//import jakarta.persistence.EntityListeners;
-//import jakarta.persistence.MappedSuperclass;
-//import lombok.Getter;
-//import org.springframework.data.annotation.CreatedBy;
-//import org.springframework.data.annotation.CreatedDate;
-//import org.springframework.data.annotation.LastModifiedBy;
-//import org.springframework.data.annotation.LastModifiedDate;
-//import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-//
-//import java.time.LocalDateTime;
-//
-//@MappedSuperclass
-//@Getter
-//@EntityListeners(AuditingEntityListener.class)
-//public abstract class BaseEntity {
-//
-//    @CreatedDate
-//    @Column(updatable = false)
-//    private LocalDateTime createAt;
-//
-//    @LastModifiedDate
-//    private LocalDateTime updateAt;
-//
-//    @CreatedBy
-//    @Column(updatable = false)
-//    private String createdBy;
-//
-//    @LastModifiedBy
-//    private String lastModifiedBy;
-//
-//}
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@MappedSuperclass
+@SuperBuilder
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+}
