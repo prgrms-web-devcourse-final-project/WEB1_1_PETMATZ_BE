4c7c02228d25267621e867aaf39c27f87f66db14
류희수 <hs201016@gmail.com>
2024-11-27 00:32:06 +0900

    Feat : 매칭 데이터 json 형식으로 redis에 저장

--- src/main/java/com/petmatz/infra/redis/config/RedisConfig.java
+++ src/main/java/com/petmatz/infra/redis/config/RedisConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
@@ -16,7 +17,9 @@ public class RedisConfig {
 
         // key, value String 으로 직렬화
         redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setValueSerializer(new StringRedisSerializer());
+//        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+
 
         return redisTemplate;
     }
