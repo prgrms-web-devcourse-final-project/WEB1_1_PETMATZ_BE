bcb5529da71b4d329d05ebe51c19c32e61f0d567
LeeJongWon <106072775+LJW22222@users.noreply.github.com>
2024-12-09 13:21:54 +0900

    fix: 랜딩페이지 버그 fix

--- src/main/java/com/petmatz/api/main_page/MainPageController.java
+++ src/main/java/com/petmatz/api/main_page/MainPageController.java
@@ -1,27 +1,19 @@
 package com.petmatz.api.main_page;
 
 import com.petmatz.api.global.dto.Response;
+import com.petmatz.api.main_page.dto.MainPagePetMissionResponse;
 import com.petmatz.common.security.utils.JwtExtractProvider;
 import com.petmatz.domain.petmission.PetMissionService;
 import com.petmatz.domain.petmission.dto.UserToPetMissionListInfo;
 import com.petmatz.domain.petmission.entity.UserToPetMissionEntity;
-import com.petmatz.domain.sosboard.SosBoard;
-import com.petmatz.domain.sosboard.SosBoardServiceImpl;
-import com.petmatz.domain.sosboard.dto.PageResponseDto;
-import com.petmatz.domain.sosboard.dto.SosBoardServiceDto;
-import com.petmatz.domain.user.entity.User;
-import com.petmatz.domain.user.repository.UserRepository;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
@@ -34,14 +26,12 @@ public class MainPageController {
 
     @Operation(summary = "랜딩 페이지 사용자 미션 조회 [ 당일 ]", description = "랜딩 페이지 사용자 미션 조회 [ 당일 ] API")
     @GetMapping
-    public Response<List<UserToPetMissionListInfo>> getUserSchedule() {
+    public Response<List<MainPagePetMissionResponse>> getUserSchedule() {
         Long userId = jwtExtractProvider.findIdFromJwt();
         List<UserToPetMissionEntity> userToPetMissionEntities = petMissionService.selectPetMissionList(userId, LocalDate.now());
-
-        List<UserToPetMissionListInfo> list = userToPetMissionEntities.stream().map(
-                userToPetMissionEntity ->UserToPetMissionListInfo.of(userToPetMissionEntity, petMissionService.selectUserToPetMissionList(String.valueOf(userToPetMissionEntity.getPetMission().getId())))
-        ).toList();
-        return Response.success(list);
+        return Response.success(userToPetMissionEntities.stream().map(
+                MainPagePetMissionResponse::of
+        ).toList());
     }
 
 
